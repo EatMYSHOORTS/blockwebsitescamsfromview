a3a65a2278db79f74b35e76dc74bf98a38b1c597
cbgames-xyz <106137185+EatMYSHOORTS@users.noreply.github.com>
2024-12-07 11:40:45 +1300

    n4l

--- extensioncreation/background.js
+++ extensioncreation/background.js
@@ -1,8 +1,8 @@
 const blockedSites = [
   { domain: "download.cnet.com", reason: "Malware Risk", redirectPage: "blocklist.html" },
-  { domain: "", reason: "Violates Media Acceptance Rules", redirectPage: "blocklist2.html" },
+  { domain: "tiktok.com", reason: "Violates Media Acceptance Rules", redirectPage: "blocklist2.html" },
   { domain: "n4l.co.nz", reason: "Violates Access to Freedom", redirectPage: "blocklist3.html"},
-];
+];  
 
 // Listen for navigation events
 chrome.webNavigation.onBeforeNavigate.addListener((details) => {

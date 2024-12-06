a512391afce72f0c8bc1eb502a0c47de632bf0b4
cbgames-xyz <106137185+EatMYSHOORTS@users.noreply.github.com>
2024-12-06 20:55:54 +1300

    download.cnet.com because of a scam on website

---
+++ runblock.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Add registry entry to block www.example.com
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Domains\download.cnet.com" /v "*" /t REG_DWORD /d 4 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Domains\download.cnet.com\www" /v "*" /t REG_DWORD /d 4 /f
+echo Website blocked successfully.
+pause

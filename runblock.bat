981fa1d71edbaf89b371bfb1f9bd9b760a981099
cbgames-xyz <106137185+EatMYSHOORTS@users.noreply.github.com>
2024-12-06 21:06:14 +1300

    if work = good

--- runblock.bat
+++ runblock.bat
@@ -1,6 +1,7 @@
 @echo off
-REM Add registry entry to block www.example.com
+REM Add registry entry to block scam websites
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Domains\https://download.cnet.com" /v "*" /t REG_DWORD /d 4 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Domains\https://download.cnet.com\www" /v "*" /t REG_DWORD /d 4 /f
-echo Website blocked successfully.
+start chrome --deny-url "https://download.cnet.com"
+echo Websites blocked successfully.
 pause

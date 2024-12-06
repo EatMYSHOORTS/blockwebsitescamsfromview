39388ae750bc05ea9164366f91f5928e99282494
cbgames-xyz <106137185+EatMYSHOORTS@users.noreply.github.com>
2024-12-06 21:16:40 +1300

    try stop it

--- runblock.bat
+++ runblock.bat
@@ -1,7 +1,24 @@
 @echo off
-REM Add registry entry to block scam websites
+REM Add registry entry to block download.cnet.com because of possible scams
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Domains\https://download.cnet.com" /v "*" /t REG_DWORD /d 4 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Domains\https://download.cnet.com\www" /v "*" /t REG_DWORD /d 4 /f
-start chrome --deny-url "https://download.cnet.com"
 echo Websites blocked successfully.
+reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Edge\URLBlocklist" /v "1" /t REG_SZ /d "https://download.cnet.com" /f
+reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Edge\URLAllowlist" /v "1" /t REG_SZ /d "*" /f
+echo Blocked webpage for Edge!
+copy policies.json "%ProgramFiles%\Mozilla Firefox\distribution\policies.json"
+echo Blocked webpage for Firefox!
+echo Start
+set "gx_path=%LOCALAPPDATA%\Programs\Opera GX"
+
+REM Check if Opera GX is installed
+if exist "%gx_path%" (
+    echo Opera GX detected. Configuring block settings...
+    rem Example of blocking via modifying hosts or adding an extension
+    echo https://download.cnet.com >> "%WINDIR%\System32\drivers\etc\hosts"
+    echo Block rule applied for Opera GX.
+) else (
+    echo Opera GX is not installed. No action taken.
+)
+
 pause
